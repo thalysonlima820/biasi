f77c44f825255e4059bdb3407b1470bff52b5d29
thalysonlima820 <thalysonlima820@gmail.com>
2023-05-15 16:15:26 -0300

    {

--- js.js
+++ js.js
@@ -42,12 +42,20 @@ switch(diahoje){
     case 1:
         if(hora >= 1 && hora<6){
             bv.innerHTML =` Boa madrugada <br>  Venha Conferir as Promoçoes de Segunda-Feira`
+            setores.innerHTML =`Promoção do setor Frios`
+            prin.innerHTML = `Segunda dos Frios`
         } else if(hora >=6 && hora <12){
             bv.innerHTML =` Bom Dia <br>  Venha Conferir as Promoçoes de Segunda-Feira`
+            setores.innerHTML =`Promoção do setor Frios`
+            prin.innerHTML = `Segunda dos Frios`
         } else if(hora >=12 && hora<18){
             bv.innerHTML =` Boa Tarde <br>  Venha Conferir as Promoçoes de Segunda-Feira`
+            setores.innerHTML =`Promoção do setor Frios`
+            prin.innerHTML = `Segunda dos Frios`
         }else if(hora>=18 && hora<=20){
             bv.innerHTML =` Boa noite <br>  Venha Conferir as Promoçoes de Segunda-Feira, Estaremos aberto ate as 19:30, Corra que ainda da tempo`
+            setores.innerHTML =`Promoção do setor Frios`
+            prin.innerHTML = `Segunda dos Frios`
         } else{
             bv.innerHTML =` Boa noite <br>  Nesse horario ja estamos fechamo, Mas fique de olho nas promoçoes de Terça-Feira`
         }
@@ -55,12 +63,20 @@ switch(diahoje){
     case 2:
         if(hora >= 1 && hora<6){
             bv.innerHTML =` Boa madrugada <br>  Venha Conferir as Promoçoes de Terça-Feira`
+            setores.innerHTML =`Promoção do setor de Limpeza`
+            prin.innerHTML = `Terça da Limpeza`
         } else if(hora >=6 && hora <12){
             bv.innerHTML =` Bom Dia <br>  Venha Conferir as Promoçoes de Terça-Feira`
+            setores.innerHTML =`Promoção do setor de Limpeza`
+            prin.innerHTML = `Terça da Limpeza`
         } else if(hora >=12 && hora<18){
             bv.innerHTML =` Boa Tarde <br>  Venha Conferir as Promoçoes de Terça-Feira`
+            setores.innerHTML =`Promoção do setor de Limpeza`
+            prin.innerHTML = `Terça da Limpeza`
         }else if(hora>=18 && hora<=19){
             bv.innerHTML =` Boa noite <br>  Venha Conferir as Promoçoes de Terça-Feira, Estaremos aberto ate as 19:30, Corra que ainda da tempo`
+            setores.innerHTML =`Promoção do setor de Limpeza`
+            prin.innerHTML = `Terça da Limpeza`
         } else{
             bv.innerHTML =` Boa noite <br>  Nesse horario ja estamos fechamo, Mas fique de olho nas promoçoes de Quarta-Feira`
         }
@@ -68,12 +84,20 @@ switch(diahoje){
     case 3:
         if(hora >= 1 && hora<6){
             bv.innerHTML =` Boa madrugada <br>  Venha Conferir as Promoçoes de Quarta-Feira`
+            setores.innerHTML =`Promoção do setor de Aves`
+            prin.innerHTML = `Quarta do Framgo`
         } else if(hora >=6 && hora <12){
             bv.innerHTML =` Bom Dia <br>  Venha Conferir as Promoçoes de Quarta-Feira`
+            setores.innerHTML =`Promoção do setor de Aves`
+            prin.innerHTML = `Quarta do Framgo`
         } else if(hora >=12 && hora<18){
             bv.innerHTML =` Boa Tarde <br>  Venha Conferir as Promoçoes de Quarta-Feira`
+            setores.innerHTML =`Promoção do setor de Aves`
+            prin.innerHTML = `Quarta do Framgo`
         }else if(hora>=18 && hora<=19){
             bv.innerHTML =` Boa noite <br>  Venha Conferir as Promoçoes de Quarta-Feira, Estaremos aberto ate as 19:30, Corra que ainda da tempo`
+            setores.innerHTML =`Promoção do setor de Aves`
+            prin.innerHTML = `Quarta do Framgo`
         } else{
             bv.innerHTML =` Boa noite <br>  Nesse horario ja estamos fechamo, Mas fique de olho nas promoçoes de Quinta-Feira`
         }
@@ -81,12 +105,20 @@ switch(diahoje){
     case 4:
         if(hora >= 1 && hora<6){
             bv.innerHTML =` Boa madrugada <br>  Venha Conferir as Promoçoes de Quinta-Feira`
+            setores.innerHTML =`Promoção do setor de Açougue`
+            prin.innerHTML = `Quinta do Filé`
         } else if(hora >=6 && hora <12){
             bv.innerHTML =` Bom Dia <br>  Venha Conferir as Promoçoes de Quinta-Feira`
+            setores.innerHTML =`Promoção do setor de Açougue`
+            prin.innerHTML = `Quinta do Filé`
         } else if(hora >=12 && hora<18){
             bv.innerHTML =` Boa Tarde <br>  Venha Conferir as Promoçoes de Quinta-Feira`
+            setores.innerHTML =`Promoção do setor de Açougue`
+            prin.innerHTML = `Quinta do Filé`
         }else if(hora>=18 && hora<=19){
             bv.innerHTML =` Boa noite <br>  Venha Conferir as Promoçoes de Quinta-Feira, Estaremos aberto ate as 19:30, Corra que ainda da tempo`
+            setores.innerHTML =`Promoção do setor de Açougue`
+            prin.innerHTML = `Quinta do Filé`
         } else{
             bv.innerHTML =` Boa noite <br>  Nesse horario ja estamos fechamo, Mas fique de olho nas promoçoes de Sexta-Feira`
         }
@@ -94,15 +126,21 @@ switch(diahoje){
     case 5:
         if(hora >= 1 && hora<6){
             bv.innerHTML =` Boa madrugada <br>  Venha Conferir as Promoçoes de Sexta-Feira`
+            setores.innerHTML =`Promoção do setor Hortifrute`
+            prin.innerHTML = `Sexta  Verde`
         } else if(hora >=6 && hora <12){
             bv.innerHTML =` Bom Dia <br>  Venha Conferir as Promoçoes de Sexta-Feira`
+            setores.innerHTML =`Promoção do setor Hortifrute`
+            prin.innerHTML = `Sexta  Verde`
         } else if(hora >=12 && hora<18){
             bv.innerHTML =` Boa Tarde <br>  Venha Conferir as Promoçoes de Sexta-Feira`
             setores.innerHTML =`Promoção do setor Hortifrute`
-            prin.innerHTML = `Sexta-Feira Verde`
+            prin.innerHTML = `Sexta  Verde`
             prin.style.color = 'green' 
         }else if(hora>=18 && hora<=20){
             bv.innerHTML =` Boa noite  <br> Venha Conferir as Promoçoes de Sexta-Feira, Estaremos aberto ate as 19:30, Corra que ainda da tempo`
+            setores.innerHTML =`Promoção do setor Hortifrute`
+            prin.innerHTML = `Sexta  Verde`
         } else{
             bv.innerHTML =` Boa noite <br>  Nesse horario ja estamos fechamo, Mas fique de olho nas promoçoes de Sabado`
         }
@@ -132,3 +170,21 @@ switch(diahoje){
 // btn.addEventListener('click', function(){
 //     window.scroll({top: window.innerHeight, behavior: 'smooth'})
 // })
+
+
+let count = 1;
+let intervalId = setInterval(nextimg, 3000);
+
+function nextimg() {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+
+  document.getElementById('slide' + count).checked = true;
+}
+
+// adiciona um event listener para o evento "click" na página
+document.addEventListener('click', function() {
+  clearInterval(intervalId); // para o intervalo
+});
